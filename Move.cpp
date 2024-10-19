d3a2884866f74efaba0d5aeb539c158379b1fc05
Abdelrahman Tarek <abdelrhmantarekmahmoud@gmail.com>
2024-10-20 01:56:16 +0300

    Move added

---
+++ Move.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+template<typename T>
+constexpr typename std::remove_reference<T>::type&& my_move(T&& arg) noexcept {
+    return static_cast<typename std::remove_reference<T>::type&&>(arg);
+}
+
+class Widget
+{
+public:
+    Widget(){std::cout << "Widget Constructor !" << std::endl;}
+    Widget(Widget&&){std::cout <<"Widget Move Constructor !"<< std::endl;}
+    Widget&& operator = (Widget&&){std::cout <<"Widget Move Assignment operator !"<< std::endl;}
+};
+int main()
+{
+    Widget w1;
+    Widget w2(my_move(w1)); //calls the move cotr
+    Widget w3;
+    w3 = my_move(w2);//calls the move assignment 
+
+}    
